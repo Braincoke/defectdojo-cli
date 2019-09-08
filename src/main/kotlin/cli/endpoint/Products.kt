778ab9a6df21754ffb66fb5dd306cb89eb1110c7
Braincoke <braincoke@protonmail.com>
2019-09-08 22:10:54 +0200

    Add products POST and PUT endpoint

--- src/main/kotlin/cli/endpoint/Products.kt
+++ src/main/kotlin/cli/endpoint/Products.kt
@@ -1,22 +1,23 @@
 package cli.endpoint
 
-import DojoConfig
-import cli.GetCommand
-import cli.GetCommandWithOrder
-import cli.getBody
+import cli.*
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.PrintMessage
-import com.github.ajalt.clikt.core.requireObject
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.types.int
-import com.google.gson.JsonSyntaxException
-import cli.TableFormatter
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
 import defectdojo.api.v1.DefectDojoAPI
+import defectdojo.api.v1.Product
+import defectdojo.api.v1.ProductType
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.inSet
 import org.kodein.di.generic.provider
+import retrofit2.Response
+import java.time.LocalDateTime
 
 class ProductCli : CliktCommand(
     name = "product",
@@ -30,7 +31,9 @@ val productModule = Kodein.Module("product") {
     bind<CliktCommand>().inSet() with provider {
         ProductCli().subcommands(
             ProductListCli(),
-            ProductsIdCli()
+            ProductsIdCli(),
+            ProductAddCli(),
+            ProductUpdateCli()
         )
     }
 }
@@ -65,5 +68,162 @@ class ProductsIdCli : GetCommand(
         val response = dojoAPI.getProduct(id)
             .execute()
         return tableFormatter.format(listOf(getBody(response)))
+    }
+}
+
+
+class ProductAddCli : PostCommand(
+    name = "add",
+    help = """Add a product"""
+) {
+    private val qName by argument("name", help = "The name of the product")
+    private val qType by argument("type", help = "The id of the product type").int()
+    private val qDescription by argument("description", help = "The description of the product")
+    private val qBusinessCriticality by option(
+        "--criticality", help = "Indicate the criticality of the product."
+    ).choice(
+        "none" to "none",
+        "very-low" to "low",
+        "low" to "low",
+        "medium" to "medium",
+        "high" to "high",
+        "very-high" to "very high"
+    )
+    private val qPlatform by option(
+        "--platform", help = "Indicate the product platform."
+    ).choice(
+        "api" to "API",
+        "desktop" to "Desktop",
+        "iot" to "Internet of Things",
+        "mobile" to "Mobile",
+        "web" to "Web"
+    )
+    private val qOrigin by option(
+        "--origin", help = "Indicate the product's origin."
+    ).choice(
+        "library" to "third party library",
+        "purchased" to "purchased",
+        "contractor" to "contractor",
+        "internal" to "internal",
+        "oss" to "open source",
+        "outsourced" to "outsourced"
+    )
+    private val qLifecycle by option(
+        "--lifecycle", help = "Indicate the product's lifecycle."
+    ).choice(
+        "construction" to "construction",
+        "production" to "production",
+        "retirement" to "retirement"
+    )
+    private val qExternal by option(
+        "--external",
+        help = "Indicate if the product is accessible by an external audience. Default is false."
+    ).flag(default = false)
+    private val qInternet by option(
+        "--internet",
+        help = "Indicate if the product is accessible from the internet. Default is false."
+    ).flag(default = false)
+    private val qRevenue by option(
+        "--revenue",
+        help = "Estimation of the application's revenue.")
+    private val qUsers by option(
+        "--users",
+        help = "Estimation of the number of user records.")
+    override fun post(dojoAPI: DefectDojoAPI): Response<Void> {
+        val product = Product(
+            name = qName,
+            created = LocalDateTime.now().toString(),
+            description = qDescription,
+            businessCriticality = qBusinessCriticality,
+            externalAudience = qExternal,
+            internetAccessible = qInternet,
+            lifecycle = qLifecycle,
+            origin = qOrigin,
+            prodType = qType.toString(),
+            revenue = qRevenue,
+            platform = qPlatform,
+            userRecords = qUsers
+        )
+        return dojoAPI.addProduct(product)
+            .execute()
+    }
+}
+
+class ProductUpdateCli : PostCommand(
+    name = "update",
+    help = """Update a product"""
+) {
+    private val qId by argument("id", help = "The identifier of the product").int()
+    private val qDescription by option("--description", help = "The description of the product")
+    private val qName by option("--name", help = "The name of the product")
+    private val qType by option("--type", help = "The id of the product type").int()
+    private val qBusinessCriticality by option(
+        "--criticality", help = "Indicate the criticality of the product."
+    ).choice(
+        "none" to "none",
+        "very-low" to "very low",
+        "low" to "low",
+        "medium" to "medium",
+        "high" to "high",
+        "very-high" to "very high"
+    )
+    private val qPlatform by option(
+        "--platform", help = "Indicate the product platform."
+    ).choice(
+        "api" to "web service",
+        "desktop" to "desktop",
+        "iot" to "iot",
+        "mobile" to "mobile",
+        "web" to "web"
+    )
+    private val qOrigin by option(
+        "--origin", help = "Indicate the product's origin."
+    ).choice(
+        "library" to "third party library",
+        "purchased" to "purchased",
+        "contractor" to "contractor",
+        "internal" to "internal",
+        "oss" to "open source",
+        "outsourced" to "outsourced"
+    )
+    private val qLifecycle by option(
+        "--lifecycle", help = "Indicate the product's lifecycle."
+    ).choice(
+        "construction" to "construction",
+        "production" to "production",
+        "retirement" to "retirement"
+    )
+    private val qExternal by option(
+        "--external",
+        help = "Indicate if the product is accessible by an external audience. Default is false."
+    ).choice("true" to true, "false" to false)
+    private val qInternet by option(
+        "--internet",
+        help = "Indicate if the product is accessible from the internet. Default is false."
+    ).choice("true" to true, "false" to false)
+    private val qRevenue by option(
+        "--revenue",
+        help = "Estimation of the application's revenue.")
+    private val qUsers by option(
+        "--users",
+        help = "Estimation of the number of user records.")
+    override fun post(dojoAPI: DefectDojoAPI): Response<Void> {
+        val product = Product(
+            id = qId,
+            name = qName,
+            created = LocalDateTime.now().toString(),
+            description = qDescription,
+            businessCriticality = qBusinessCriticality,
+            externalAudience = qExternal,
+            internetAccessible = qInternet,
+            lifecycle = qLifecycle,
+            origin = qOrigin,
+            prodType = if (qType == null) { null } else { qType.toString() } ,
+            revenue = qRevenue,
+            platform = qPlatform,
+            userRecords = qUsers
+        )
+        return dojoAPI.updateProduct(qId, product)
+            .execute()
     }
 }
